608a654436cf21eecd924a5f442168d1d6365977
Mark R <mirubenstein@gmail.com>
2018-01-14 11:29:39 -0800

    Update based on review and switch from class method to instance methods.

--- pangram.rb
+++ pangram.rb
@@ -1,16 +1,20 @@
 require 'set'
 
-module Italian
-  ALPHABET = %w[A B C D E F G H I L M N O P Q R S T U V Z].freeze
-end
-
-module Latin
-  ALPHABET = %w[A B C D E F G H I J K L M N O P Q R S T U V W X Y Z].freeze
-end
-
 class Pangram
-  def self.pangram?(phrase, language = Latin)
-    Set.new(language::ALPHABET).subset? phrase.upcase.chars.to_set
+  def pangram?(phrase)
+    Set.new(alphabet).subset? phrase.upcase.chars.to_set
+  end
+end
+
+class Italian < Pangram
+  def alphabet
+    %w[A B C D E F G H I L M N O P Q R S T U V Z].freeze
+  end
+end
+
+class Latin < Pangram
+  def alphabet
+    %w[A B C D E F G H I J K L M N O P Q R S T U V W X Y Z].freeze
   end
 end
 
